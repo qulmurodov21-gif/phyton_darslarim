f11cce6bef08cfcce503cd33ba61c1d129318e18
qulmurodov21-gif <qulmurodov21@gmail.com>
2026-07-07 19:55:37 +0500

    yangi dars
    daxshat

---
+++ untitled0.py
@@ -0,0 +1,64 @@
+#a={'ism':'Azizbek', 'familiya':'Bekmirzayev', 'yosh':22, 'maktab':279}
+#print(a.items())
+
+#for kalit, qiymat in a.items():
+#    print(f"Kalit {kalit}")
+#    print(f"Qiymat {qiymat}\n")
+
+#telefonlar={
+#    'Ali':'Ipone_17pro',
+#    'Vali':'Ipone_17',
+#    'Soli':'Ipone_17promax',
+#    "G'ani":'Ipone_17'
+#    }
+#for i,t in telefonlar.items():
+#    print(f"{i}ning telefoni{t}")
+
+#mahsulotlar={
+#    'heldersholder':10000,
+#    'erkaklarshamponi':8000,
+#    'eveybaby':67000
+#    }
+#print(mahsulotlar.keys())
+
+#print("Do'kondagi mahsulotlar:")
+#for mahsulot in mahsulotlar:
+#    print(mahsulot.title())
+
+#bozorlik=['heldersholder',"go'sht",'olma','eveybaby']
+#for mahsulot in mahsulotlar:
+#    if mahsulot in bozorlik:
+#        print(f"{mahsulot.title()} {mahsulotlar[mahsulot]} so'm")
+#
+#for q in bozorlik:
+#    if q not in mahsulotlar:
+#        print(f"Iltimos do'koningizga {q} ham olib kelinglar savdo kuchayadi")
+
+#print("Do'konimizdagi mahsulotlar")
+#for mahsulot in sorted(mahsulotlar):
+#    print(mahsulot.title())  
+
+#print(telefonlar.values())     
+#print("Bratishkalarni 'halol' ishlab topgan telefonlari")
+#for bratish in telefonlar.values():
+#    print(bratish)
+
+#print("O'rto'larim shu telefonlarni ishlatishadi")
+#for k in telefonlar.values():
+#    print(k)
+#    
+#print("O'rto'larim shu telefonlarni ishlatishadi")
+#for k in set(telefonlar.values()):
+#    print(k)
+
+toys={"ball","car","ball","phone"}
+
+
+
+
+
+    
+    
+    
+    
+
