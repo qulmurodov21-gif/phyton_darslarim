51113489cae90cb50b7a34c8d9024508a046ecd3
qulmurodov21-gif <qulmurodov21@gmail.com>
2026-07-08 23:34:02 +0500

    1-darsim

---
+++ hello-world.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 23 02:43:29 2026
+
+@author: odilbek
+"""
+print("Hello world!")
+print("shu gapizga manavuni va manucuni yebsiz")
