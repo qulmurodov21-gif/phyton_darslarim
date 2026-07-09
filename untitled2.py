e98811b98d671979cc88374830767edd3387641d
qulmurodov21-gif <qulmurodov21@gmail.com>
2026-07-09 20:00:40 +0500

    while

---
+++ untitled2.py
@@ -0,0 +1,61 @@
+#ism=input("Ismingiz kim?")
+#savol=f"Salom, {ism.title()}. Yoshingiz nechida? "
+#yosh=input(savol)
+#yosh=int(yosh)
+#height=input("Bo'yingiz necha metr?")
+#height=float(height)
+
+#son=1
+#while son<=5:
+#    print(son, end=' ')
+#    son=son+1
+#print("Dastur tugadi")
+
+#print("Bu sonning kubini qaytaruvchi dastur.")
+#savol="Istalgan son kiriting "
+#savol+= "(dasturni to'xtatish uchun 'Esc' deb yozing):"
+#qiymat=''
+#while qiymat !='Esc':
+#    qiymat=(input(savol))
+#    if qiymat != 'Esc':
+#        print(float(qiymat)**3)
+#print("Dastur tugadi")
+    
+#print("Bu sonning kubini qaytaruvchi dastur.")
+#savol="Istalgan son kiriting "
+#savol+= "(dasturni to'xtatish uchun 'Esc' deb yozing):"
+#ishora=True
+#while ishora:
+#    qiymat=input(savol)
+#    if qiymat=='Esc':
+#        ishora=False
+#    else:
+#        print(float(qiymat)**3)
+#print("Dastur to'xtadi")
+
+#print("Bu sonning kubini qaytaruvchi dastur.")
+#savol="Istalgan son kiriting "
+#savol+= "(dasturni to'xtatish uchun 'Esc' deb yozing):"
+#
+#while True:
+#    qiymat=input(savol)
+#    if qiymat=='Esc':
+#        break
+#    else:
+#        print(float(qiymat)**3)
+#print("Dastur to'xtadi")
+
+#sonlar=list(range(1,10))
+#for son in sonlar:
+#    if son==50:
+#        continue
+ #   print(f"{son}ning kvadrati {son**2} ga teng")
+ 
+son=0
+while son<18:
+    if son%2==0:
+        continue
+    else:
+        print(son)
+son+=1
+    
