20dd4cf9dead4e99459e7cbd76b56ece826d5577
qulmurodov21-gif <qulmurodov21@gmail.com>
2026-07-12 16:21:40 +0500

    funksiya tuzish

---
+++ untitled4.py
@@ -0,0 +1,6 @@
+#print("Eng sodda funksiya")
+
+def yosh_hisobla(ism,tugilgan_yil):
+    """Foydalanuvchi yoshini hisoblaydigan dastur"""
+    print(f"{ism.title()} {2026-tugilgan_yil} yoshda")
+    
