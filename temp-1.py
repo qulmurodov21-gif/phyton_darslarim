c5aaec721e28b7d0759ef79efe224cd0c98d44d1
qulmurodov21-gif <qulmurodov21@gmail.com>
2026-07-12 23:35:31 +0500

    Create temp-1.py

---
+++ temp-1.py
@@ -0,0 +1,23 @@
+#mashina_1={"nomi":"Nexia 2","rangi":"Qora"}
+#print(mashina_1['rangi'])
+#print(mashina_1['nomi'])
+
+#en_uz={"six_seven":"67", "potato":"kartoshka", "banana":"banan"}
+
+#mevalar={"shaftoli":10000, "zardoli":25000, "olma":6700}
+#print(f"Olmani narxi {mevalar['olma']}so'm")
+
+#a={'ism':'azizbek', 'yosh':'16', 't_kun':'14-may'}
+#print(f"Akamni ismi {a['ism']}, yoshi {a['yosh']}, tug'ilgan kuni{a['t_kun']}.")
+
+#a['maktab'] =279
+#a['fakultet'] ="yo'q"
+
+#n={}
+
+telefonlar={
+    'aziz':'samsung',
+    'odil':'honor',
+    'nunush':"yo'q",
+    'nodira':"redmi"
+    }
