efac19d739139380e7eaac2eb63c8111bb133b25
qulmurodov21-gif <qulmurodov21@gmail.com>
2026-09-06 18:12:25 +0500

    Delete untitled4.py

--- untitled4.py
+++
@@ -1,6 +0,0 @@
-#print("Eng sodda funksiya")
-
-def yosh_hisobla(ism,tugilgan_yil):
-    """Foydalanuvchi yoshini hisoblaydigan dastur"""
-    print(f"{ism.title()} {2026-tugilgan_yil} yoshda")
-    
