8f80112b6d48bb0215e7bd2825c7f7eff895bc34
qulmurodov21-gif <qulmurodov21@gmail.com>
2026-09-10 23:18:28 +0500

    O'zgaruvchilar
    Bunda o'zgaruvchilar haqida boshlang'ich tushuntirish

---
+++ O'zgaruvchilar.py
@@ -0,0 +1,41 @@
+#3-dars. O'zgaruvchilar
+#Sana=>10.09.2026
+#Muallif:Bekmirzayev Odilbek
+
+#ism = "Odilbek"
+#t_yili = 2012 
+#print(ism)
+#print(t_yili)
+
+#ism = "Odilbek"
+#print(ism)
+#ism = "Azizbek"
+#print(ism)
+
+
+#a=6
+#b=7
+#c=(a+b)**2
+#print(c)
+
+
+
+#ism_sharif="Odilbek Bekmirzayev"
+
+
+#t_yili=2012
+#t=2012
+
+#pyhton_ilk_gaplar="Hello world!"
+#print(pyhton_ilk_gaplar)
+
+
+#xabar="Hozir SH.Mirziyoyev prezident O'zbekistonda"
+
+#class=5
+#print(class)
+
+radius = 5
+pi = 3.14159
+aylana_yuzi = pi * radius**2
+print("Radiusi" , radius, "ga teng aylananing yuzi=", aylana_yuzi)
