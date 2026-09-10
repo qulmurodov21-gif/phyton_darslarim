96a3094cf7a097b98c8481c3aec8a441fb995fea
qulmurodov21-gif <qulmurodov21@gmail.com>
2026-09-10 20:18:37 +0500

    print 2
    PRINT(), funksiyasi arifmetik amallar va ularni matn bilan birga chiqarish,  va SyntexError haqida

---
+++ print(), Arifmetik amallar va Sinteks.py
@@ -0,0 +1,33 @@
+#2-dars. PRINT() funksiyasi, Arifmetik amallar va Syntex xatoliklar
+#Sana=>10.09.2026
+#Muallif: Bekmirzayev Odilbek
+
+# (#) bu o'sha qatordagi kod va boshqa narsalarni ishdan ciqaradi
+
+#print("Hello world!") #Izoh: Hello world degan jumlani konsolga chiqaadi
+#print("Assalomu alaykum")
+#print(Hayrli tong!)
+#print('Hayrli tong!')
+#print("Hayrli tong!")
+
+
+#print('Men "ACER" markali noutbook ishlataman')
+#print("Men \"ACER\" markali noutbook ishlataman")
+
+#print("""Odami ersang, demagil odami,
+#Oniki, yo'q xalq g'amidin g'ami""")
+
+#print("Odami ersang, demagil odami, \nOniki, yo'q xalq g'amidin g'ami")
+#print('Odami ersang, demagil odami, \nOniki, yo\'q xalq g\'amidin g\'ami')
+#print("Odami ersang, demagil odami, \nOniki, yo'q xalq g'amidin g'ami")
+
+#print(2+4*2)
+#print(19/5)
+#print(20/5)
+#print(16//4)
+#print(67/6)
+#print(67//6)
+#print(2**4)
+
+#print("Oltmish yettini kvadrati", 67**2, "ga teng")
+print('3x3=',3*3)
