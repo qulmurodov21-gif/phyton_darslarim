e0103f4e33b63c9b1beb19d934a1dfcb45ae5b7a
qulmurodov21-gif <qulmurodov21@gmail.com>
2026-09-10 20:24:02 +0500

    Update print(), Arifmetik amallar va Sinteks.py

--- print(), Arifmetik amallar va Sinteks.py
+++ print(), Arifmetik amallar va Sinteks.py
@@ -30,4 +30,23 @@
 #print(2**4)
 
 #print("Oltmish yettini kvadrati", 67**2, "ga teng")
-print('3x3=',3*3)
+#print('3x3=',3*3)
+
+
+#AMALIYOT
+print("\"Nexia\", \"Tico\", 'Damas' ko'rganlar qilar havas")
+print('5 ning 4-darajasi', 5**4)
+print("22 ni 4 ga bo'lganda qancha qoldiq", 22%4)
+print("Tomonlari 125 ga teng kvadratning yuzi", 125*125, "ga, perimetri", 4*125, "teng")
+print('Diametri 12 ga teng bo\'lgan doiraning yuzi', 3.14*(12/2)**2, 'ga teng')
+print("Katetlari 6 va 7 bo'lgan to'g'ri burchakli uchburchakning gipotenuzasi", (6**2+7**2)**(1/2))
+
+
+
+
+
+
+
+
+
+
