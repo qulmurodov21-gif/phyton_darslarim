14f8aef099beb2bea2fcc42142c1300319e99c7f
qulmurodov21-gif <qulmurodov21@gmail.com>
2026-09-10 18:47:37 +0500

    print funksiyasi
    Hello world

---
+++ hello_world.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 10 18:23:07 2026
+
+@author: odilbek
+"""
+
+print("Hello world!")
+print(7*8-67)
+
+
+
+#Amaliyot
+print('Assalom alaykum')
+print("Hayrli tong!")
+print(2+4*2)
+print(19/5)
+print(2**4)
